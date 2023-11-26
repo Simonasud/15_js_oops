56d5f67af308d6d06150ddf29318e238336bcc51
Simona Sud <ssimona.sud@gmail.com>
2023-11-26 19:36:15 +0200

    h1 changed

--- js/uzduotys.js
+++ js/uzduotys.js
@@ -1,2 +1,6 @@
 "use strict";
 console.log("uzduotys.js file was loaded");
+
+const titleEl = document.querySelector("#title");
+console.log("titleEl ===", titleEl);
+titleEl.textContent = "Pakeitimai padaryti";
